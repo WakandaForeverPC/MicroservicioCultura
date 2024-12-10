b4773c7541c0178e6c8b6a397fc057a5b1f007e9
Samuu10 <svallmun@myuax.com>
2024-12-10 18:23:12 +0100

    Initial commit

---
+++ src/main/java/com/proyecto/microserviciocultura/MicroservicioCulturaApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.microserviciocultura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioCulturaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicioCulturaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/proyecto/microserviciocultura/MicroservicioCulturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.microserviciocultura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioCulturaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
